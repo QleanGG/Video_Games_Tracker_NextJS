a58a2942bf467645da0f03aa04a2692a66652764
QleanGG <guyguz1@gmail.com>
2024-06-13 16:19:17 +0300

    fix speedinsights import

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
-import { withSpeedInsights } from '@vercel/speed-insights';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const nextConfig = {
   reactStrictMode: true,
@@ -35,6 +35,6 @@ const bundleAnalyzerConfig = {
   enabled: process.env.ANALYZE === 'false',
 };
 
-export default withSpeedInsights(
+export default SpeedInsights(
   withBundleAnalyzer(bundleAnalyzerConfig)(nextConfig)
 );
