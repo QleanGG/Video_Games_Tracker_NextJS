8991f0eac957193113b21edc8a33dad853bb9807
QleanGG <guyguz1@gmail.com>
2024-06-14 10:39:15 +0300

    fix hadnling of 401 error, add preconnect to backend server

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,16 +1,23 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import mainApi from '@/api/apiAxios';
 import { User } from '@/types';
+import axios from 'axios';
 
 
-const fetchUser = async (): Promise<User> => {
-	const { data } = await mainApi.get<User>('/auth/user');
-	return data;
+const fetchUser = async (): Promise<User | null> => {
+  try {
+    const { data } = await mainApi.get<User>('/auth/user');
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 403) {
+      return null;
+    }
+    throw error; // Re-throw if it's not a 403 error
+  }
 };
 
-export const useUser = () => {
-
-  return useQuery<User, Error>({
+export const useUser = (): UseQueryResult<User | null, Error> => {
+  return useQuery<User | null, Error>({
     queryKey: ['user'],
     queryFn: fetchUser,
     staleTime: 1000 * 60 * 10,
